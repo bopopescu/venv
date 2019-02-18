a89c0c8d6972aaec8e77afdfd59ef6f2515c7b7e
eu9327 <e.blaghodir@gmail.com>
2019-02-18 23:31:30 +0200

    added students2.py

---
+++ students2.py
@@ -0,0 +1,18 @@
+"""
+UPDATED by eu9327
+lab from onlie
+"""
+
+marksheet = []
+scorelist=[]
+for i in range(int(input())):
+        name = input()
+        score = float(input())
+
+        marksheet += [[name, score]]
+        scorelist += [score]
+b = sorted(list(set(scorelist)))[1]
+print(b)
+for a, c in sorted(marksheet):
+    if c == b:
+        print(a)
